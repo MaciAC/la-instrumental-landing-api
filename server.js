8863322a904f5e203c450e6798688862099767fc
MaciAC <macia.ac@gmail.com>
2025-11-27 12:52:41 +0100

    cors

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.set('trust proxy', 1);
 
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
